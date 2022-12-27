51ac7274c60e8bb32661ba3185926f50851dd583
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2022-12-27 23:30:36 +0530

    searchbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App() {
+  
   return (
+  <>
     <div className="App">
       <h1>Search Anime Characters</h1>
+      <input 
+        type="text"
+        placeholder='Search here'
+        className='SearchInput'
+      />
     </div>
+  </>
+    
   );
 }
 export default App;
